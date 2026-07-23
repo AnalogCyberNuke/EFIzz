cc78de3106e1a7632f4ba16b2ab64b07608969a9
Anticheat Intel Solutions i.o <contact@anticheat.solutions>
2026-07-22 18:22:01 -0700

    create todos

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    // TODO init WHP to 64 bit identity paged
+    // map driver
+    // make harness
+    // trace entry point
+    // log ACPI registrations and NVVARS
 }
